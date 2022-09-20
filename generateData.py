5a49321db7a4076d20b7499b1cd952bf04d48771
Aaron Haag <aaron.haag@siemen.com>
2022-09-20 13:22:30 +0200

    - paper config - alphak, vavailiabilities in file

--- generateData.py
+++ generateData.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pickle
 from scipy.stats import truncnorm
@@ -9,13 +11,8 @@ import random
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
 from copy import copy
-import geopandas as gpd
-import pandas as pd
 import numpy as np
-import haversine as hs
-from haversine import Unit
-from shapely.geometry import Polygon
-import tqdm
+
 
 def vis_random_location_process(num_pt, geo_df):
     """
@@ -81,30 +78,42 @@ def vis_random_location_process(num_pt, geo_df):
     #ax2.figure.savefig("step2.png", dpi=150)
     #ax3.figure.savefig("step3.png", dpi=150)
     ax4.figure.savefig("map.png", dpi=150)
-    #plt.show()
+    plt.show()
 
     return list(zip(x_2, y_2))
 
     #hs.haversine((x_2[0], y_2[0]), (x_2[1], y_2[1]), unit=Unit.METERS)
 
-plz_shape_df = gpd.read_file('plz-5stellig.shp', dtype={'plz': str})
-plz_region_df = pd.read_csv('plz-5stellig-daten.csv', sep=',', dtype={'plz': str})
-# plz_region_df.drop('osm_id', axis=1, inplace=True)
-germany_df = pd.merge(left=plz_shape_df, right=plz_region_df, on='plz', how='inner')
-düsseldorf_df = germany_df.query('plz == "80636"')
-düsseldorf_geo = düsseldorf_df["geometry"]
+#plz_shape_df = gpd.read_file('plz-5stellig.shp', dtype={'plz': str})
+#plz_region_df = pd.read_csv('plz-5stellig-daten.csv', sep=',', dtype={'plz': str})
+#germany_df = pd.merge(left=plz_shape_df, right=plz_region_df, on='plz', how='inner')
+#düsseldorf_df = germany_df.query('plz == "80636"')
+#düsseldorf_geo = düsseldorf_df["geometry"]
 
 
 
-customName = "random"
+customName = "random_hhoherPrice"
 
 noOfTasks = 10
 noOfUsers = 15
 
+pickelFileName = "data/" + str(noOfTasks) + str(noOfUsers) + str(customName)
+
+beta = 1 # -->oo : globaliy ranked arms
+x_i = np.random.uniform(noOfTasks)
+epsilon_i_k = np.random.logistic(0,1,size=[noOfUsers,noOfTasks])
+mus = beta*x_i + epsilon_i_k
+mu= np.zeros([noOfUsers, noOfTasks])
+mu[0] = np.arange(0,noOfTasks)
+#for user in range(0, noOfUsers):
+#    for task in range(0, noOfTasks):
+#        mu[user][task] = np.sum(mus[user][:] <= mus[user][task])
+for user in range(1,noOfUsers):
+    mu[user] = np.roll(mu[user-1],1)
 
 #mu = np.ones([noOfUsers, noOfTasks])#np.array([[1,0],[0,1]])
 
-#task_duration = mu + 1 + np.round(np.random.random([noOfUsers, noOfTasks]),5)
+#task_duration = mu + 1 + np.ro1und(np.random.random([noOfUsers, noOfTasks]),5)
 #task_duration[:, 2] = np.array([4.5, 4.6, 4.2, 4.4, 4])
 #task_duration[4, :] = np.array([0.3, 0.1, 4, 0.5, 0.2])
 
@@ -112,34 +121,61 @@ noOfUsers = 15
 #np.random.shuffle(mcsp_utility.flat)
 #mcsp_utility = mcsp_utility + np.round(np.random.random([noOfUsers, noOfTasks]),5)
 
-upload_speed = [20,25,30]#[20,30,50,40.99,30.1,20.1,19.9]
+upload_speed = [30,40,50]#[20,30,50,40.99,30.1,20.1,19.9]
 processing_speed = [50,60,55]#[12,10,2,8.1,8.9,9,9.1,12.1]
-task_sizes = [1,3,7]# [1,5,3,2,4]
+task_sizes = np.linspace(1,7,40)#, size=noOfTasks)#[1,3,7]# [1,5,3,2,4]
 
-revenuePerMbit = 0.1 # revenue fopr mcsp: €/Mbit for mcsp
-costPerSecond = 0.4
+revenuePerMbit = 0.09 # revenue fopr mcsp: €/Mbit for mcsp
+costPerSecond = 0.02 # cost for users:   €/sec for users
+
+T = 2000
 
 # for tests:
 maxMarge = 0.1
-deadline = 0.8
+deadline = 1
 pMoreThan = 0.8
 
 # get distances
-locationTupples = vis_random_location_process(noOfUsers+noOfTasks, düsseldorf_df)
+#locationTupples = vis_random_location_process(noOfUsers+noOfTasks, düsseldorf_df)
 # first locations are the tasks
-locationOfTasks = locationTupples[:noOfTasks]
-locationOfUsers = locationTupples[noOfTasks:]
-distances = np.ones([noOfUsers,noOfTasks]) #+ 0.5*np.random.random([noOfUsers, noOfTasks])
+#locationOfTasks = locationTupples[:noOfTasks]
+#locationOfUsers = locationTupples[noOfTasks:]
+distances = np.zeros([noOfUsers,noOfTasks])
 #for i in range(noOfUsers):
 #    for j in range(noOfTasks):
-#        distances[i,j] = hs.haversine(locationOfUsers[i], locationOfTasks[j], unit=Unit.KILOMETERS)
-alpha_k = np.random.choice(task_sizes, noOfTasks)
+#        distances[i,j] = np.random.randint(0,1) #hs.haversine(locationOfUsers[i], locationOfTasks[j], unit=Unit.KILOMETERS)
+
+
+# caluclate alpha k
+alphakFileName = "data/" + str(noOfTasks) + "_auto_" + str(customName) + "alphak" + ".pkl"
+if os.path.isfile(alphakFileName):
+    print("alphak exists")
+    with open(alphakFileName, 'rb') as f:
+        alpha_k = pickle.load(f)
+else:
+    print("recalculate alphak")
+    alpha_k = np.random.choice(task_sizes, noOfTasks,
+                               replace=False)  # np.random.uniform(low=10, high=40, size=noOfTasks)#np.random.choice(task_sizes, noOfTasks)
+    with open(alphakFileName, 'wb') as f:
+        pickle.dump(alpha_k, f)
+# calculate availabilities
+availabilitiesFileName = "data/" + str(noOfTasks) + "_auto_" + str(customName) + "availabilities" + ".pkl"
+if os.path.isfile(availabilitiesFileName):
+    print("availabilities exists")
+    with open(availabilitiesFileName, 'rb') as f:
+        availabilities = pickle.load(f)
+else:
+    print("recalculate availabilities")
+    availabilities = np.random.binomial(size=[noOfTasks,T], n=1, p= 0.7)
+    with open(availabilitiesFileName, 'wb') as f:
+        pickle.dump(availabilities, f)
+
+
 mcsp_utility_without_revenue = 1/(1+distances)*alpha_k
 beta_i = np.random.choice(processing_speed, noOfUsers)
-t_processing = (1/np.reshape(beta_i, [-1, 1])) * (np.ones([noOfUsers, noOfTasks]) + 0.001*np.random.random([noOfTasks])) * alpha_k
-gamma_i = np.random.uniform(low=30, high=90, size=noOfUsers)#np.random.choice(upload_speed, noOfUsers)#np.random.uniform(low=30, high=40, size=noOfUsers)#np.random.uniform(low=10, high=40, size=noOfUsers)#np.random.choice(upload_speed, noOfUsers)
-t_upload = (1/np.reshape(gamma_i, [-1, 1])) * (np.ones([noOfUsers, noOfTasks]) + 0.001*np.random.random([noOfUsers, noOfTasks])) * alpha_k
-#t_upload *= np.random.random([noOfTasks])
+t_processing = (1/np.reshape(beta_i, [-1, 1])) * (np.ones([noOfUsers, noOfTasks]) + 0*np.random.random([noOfUsers, noOfTasks])) * alpha_k
+gamma_i = np.random.uniform(low=10, high=40, size=noOfUsers)#np.random.choice(upload_speed, noOfUsers) #gamma_i = np.random.uniform(low=10, high=40, size=noOfUsers)#
+t_upload = (1/np.reshape(gamma_i, [-1, 1])) * (np.ones([noOfUsers, noOfTasks]) + 0*np.random.random([noOfUsers, noOfTasks])) * alpha_k
 
 #np.random.shuffle(mcsp_utility_without_revenue)
 
@@ -150,20 +186,28 @@ t_upload = (1/np.reshape(gamma_i, [-1, 1])) * (np.ones([noOfUsers, noOfTasks]) +
 for i in range(0, noOfTasks):
     _, counts = np.unique(t_processing[:,i] + t_upload[:,i] + mcsp_utility_without_revenue[:,i]*revenuePerMbit, return_counts=True)
     if not(counts == 1).all():
-        raise ValueError("no strict ordering of users"  + str(t_processing[:,i] + t_upload[:,i]) + str(mcsp_utility_without_revenue*revenuePerMbit))
+        raise ValueError("no strict ordering of users"  + str(t_processing[:,i] + t_upload[:,i]) + str(-1*t_processing - t_upload + mcsp_utility_without_revenue*revenuePerMbit))
 
 # test for max value
 if pMoreThan > np.mean(np.mean(mcsp_utility_without_revenue*revenuePerMbit > (1+maxMarge)*(t_processing + t_upload)*costPerSecond)):
+    print("task duration (processing+upload):")
+    print(t_processing + t_upload)
+    print("task cost :")
+    print((1 + maxMarge) * (t_processing + t_upload) * costPerSecond)
     raise ValueError("system not profitable" + str(maxMarge*(t_processing + t_upload)*costPerSecond) + str(mcsp_utility_without_revenue*revenuePerMbit))
 if pMoreThan > np.mean(np.mean(deadline > (t_processing + t_upload))):
+    print("task duration (processing+upload):")
+    print(t_processing + t_upload)
+    print("task cost :")
+    print((1 + maxMarge) * (t_processing + t_upload) * costPerSecond)
     raise ValueError("deadline too small" + str((t_processing + t_upload))  + " Deadline: " +str(deadline) + " pUnderDeadline: " + str(np.mean(np.mean(deadline > (t_processing + t_upload)))))
 
 print("task duration (processing+upload):")
 print(t_processing + t_upload)
 print("task cost :")
 print((1+maxMarge)*(t_processing + t_upload)*costPerSecond)
-print("mcsp utility with prices:")
-print(mcsp_utility_without_revenue*revenuePerMbit)
+print("mcsp utility without prices and revenue:")
+print(mcsp_utility_without_revenue)
 print("pUnderDeadline:")
 print(np.mean(np.mean(deadline > (t_processing + t_upload))))
 print("pProfitable:")
@@ -173,7 +217,6 @@ print(np.mean(np.mean(mcsp_utility_without_revenue*revenuePerMbit > (1+maxMarge)
 #estimated_task_duration = np.zeros([noOfUsers,noOfTasks])# + np.random.rand(noOfUsers,noOfTasks)
 
 
-pickelFileName = "data/" + str(noOfTasks) + str(noOfUsers) + str(customName)
 data = [t_processing, t_upload, mcsp_utility_without_revenue]
 # Saving the objects:
 with open(pickelFileName + ".pkl", 'wb') as f:  # Python 3: open(..., 'wb')
