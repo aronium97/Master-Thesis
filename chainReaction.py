03c6ff484fe60050c8d5eea57996be4d9592b2ae
Aaron Haag <aaron.haag@siemen.com>
2022-10-27 15:19:44 +0200

    - added chain reaction duration info

--- chainReaction.py
+++ chainReaction.py
@@ -6,25 +6,27 @@ def getHighestImpact(users_of_tasks_pessimal_stablematch_e, taskDuration):
     # build the tree
     highestDamage = 0
     taskWithHighestDamage = np.nan
+    reactionDuration = 0
     for iTask in range(np.size(taskDuration,1)):
         # user looses the task
         users_of_tasks_pessimal_stablematch = np.copy(users_of_tasks_pessimal_stablematch_e)
         userWhichLoosesTask = users_of_tasks_pessimal_stablematch[iTask]
         if takeFailureIntoLoss:
-            damage = taskDuration[userWhichLoosesTask,iTask]
+            damage_i = taskDuration[userWhichLoosesTask,iTask]
         else:
-            damage = 0
+            damage_i = 0
         # to which task will the user travel in the next step?
         np.delete(users_of_tasks_pessimal_stablematch, iTask)
-        damage = chainReaction(userWhichLoosesTask, damage, users_of_tasks_pessimal_stablematch, taskDuration)
-        if damage > highestDamage:
-            highestDamage = damage
+        damage_i, reactionDuration_i = chainReaction(userWhichLoosesTask, damage_i, 0, users_of_tasks_pessimal_stablematch, taskDuration)
+        if damage_i > highestDamage:
+            highestDamage = damage_i
             taskWithHighestDamage = iTask
+            reactionDuration = reactionDuration_i
 
-    return highestDamage, taskWithHighestDamage
+    return {"highestDamage": highestDamage, "reactionDuration:": reactionDuration, "taskWithHighestDamage:": taskWithHighestDamage}
 
 
-def chainReaction(user, damage, users_of_tasks_pessimal_stablematch, taskDuration_n):
+def chainReaction(user, damage, reaction_duration, users_of_tasks_pessimal_stablematch, taskDuration_n):
     taskDuration = np.copy(taskDuration_n)
     # find new task for user
     task_durations_user = taskDuration[user,:]
@@ -43,10 +45,10 @@ def chainReaction(user, damage, users_of_tasks_pessimal_stablematch, taskDuratio
             # update user task match
             users_of_tasks_pessimal_stablematch[iTaskToCheck] = user
             # continue with chain reaction
-            damage = chainReaction(userFromWhomIsStolen, damage, users_of_tasks_pessimal_stablematch, taskDuration)
-            return damage
+            damage, reaction_duration = chainReaction(userFromWhomIsStolen, damage, 1+ reaction_duration, users_of_tasks_pessimal_stablematch, taskDuration)
+            return damage, reaction_duration
     # no more stealing possible
-    return damage
+    return damage, reaction_duration
 
-chainReaction(0, 0, np.array([2,1]), np.array([[1,2],[1.1,2.2],[1.4,1.9]]))
+chainReaction(0, 0, 0, np.array([2,1]), np.array([[1,2],[1.1,2.2],[1.4,1.9]]))
 print(getHighestImpact(np.array([2,1,0]), np.array([[1,2,3],[1.1,2.2,3.3],[1.4,1.9,3.6]])))
